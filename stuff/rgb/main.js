820561a3e2f05845894ed51070c6f82473a6ee0d
hehe9090909 <158105173+hehe9090909@users.noreply.github.com>
2025-07-17 01:30:06 +0300

    ...

---
+++ stuff/rgb/main.js
@@ -0,0 +1,12 @@
+const input_1 = document.getElementById("in1")
+const output_1 = document.getElementById("out1")
+const output_2 = document.getElementById("out2")
+const generate_1 = document.getElementById("gen1")
+const copy_1 = document.getElementById("copy1")
+generate_1.onclick(() => {
+    output_1.style.backgroundColor = input_1.value
+    output_2.innerText = input_1.value
+})
+copy_1.onclick(() => {
+navigator.clipboard.writeText(input_1.value)
+})
