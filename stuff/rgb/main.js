21225f57454be986f03ec4fe2112b714f33f49d5
hehe9090909 <158105173+hehe9090909@users.noreply.github.com>
2025-07-17 01:41:53 +0300

    ...

--- stuff/rgb/main.js
+++ stuff/rgb/main.js
@@ -4,7 +4,7 @@ const output_2 = document.getElementById("out2")
 const generate_1 = document.getElementById("gen1")
 const copy_1 = document.getElementById("copy1")
 generate_1.addEventListener("click", () => {
-    output_1.style.backgroundColor = input_1.value
+    output_1.style["background-color"] = input_1.value
     output_2.innerText = input_1.value
 })
 copy_1.addEventListener("click", () => {
